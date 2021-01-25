5d462b0b52af3a298dd0429af218a1c0cb6009b5
yash270998 <yashsomani2007@gmail.com>
2021-01-25 11:58:30 +0000

    If File not found throwing error 404

--- web-app/routes/singleTicket.js
+++ web-app/routes/singleTicket.js
@@ -15,7 +15,7 @@ function singleTicket(req, res) {
  axios
  .get('https://' + process.env.SUBDOMAIN + '.zendesk.com/api/v2/tickets/' + req.params.ticketId, config)
  .then(function (response) {
-   
+
 /* Using the response form get Request and rendering it into the singleTicket.ejs page  */
 ejs
 .renderFile('./views/singleTicket.ejs', response.data, function (err, result) {
@@ -24,6 +24,25 @@ ejs
       .status(200)
       .send(result // render or error
       );
+/* If File not found throwing error 404 */ 
 
+    } else {
+      res
+        .status(404)
+        .send(`'<link rel=\'stylesheet\' href=\'/css/style.css\'/>
+                  <h1 class="error-heading">
+                    404 Page Not Found
+                  </h1> '`);
+    }
+  });
+})
+    .catch(function (error) {
+      res
+        .status(404)
+        .send(`'<link rel=\'stylesheet\' href=\'/css/style.css\' >
+                <h1 class="error-heading">
+                    400 invalid ticket id requested or API not available
+                </h1>'`);
+    });
 }
 exports.singleTicket = singleTicket;
